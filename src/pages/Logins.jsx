10bacc8e4bbf6196b158568e77365ec551ed1e56
JLB <juanlb1844@gmail.com>
2021-05-17 17:43:08 -0500

    x

--- src/pages/Logins.jsx
+++ src/pages/Logins.jsx
@@ -40,6 +40,67 @@ const Login = () => {
       alert("cargado"); 
   }; 
 
+
+  const googleStart = () => {
+     var GoogleAuth;
+  var SCOPE = 'https://www.googleapis.com/auth/drive.metadata.readonly';
+  function handleClientLoad() {
+    // Load the API's client and auth2 modules.
+    // Call the initClient function after the modules load.
+    gapi.load('client:auth2', initClient);
+  }
+
+  function initClient() {
+    // In practice, your app can retrieve one or more discovery documents.
+    var discoveryUrl = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest';
+ 
+    gapi.client.init({
+        'apiKey': 'AIzaSyBbW99HNi4hkZdpNC381-RRq63MXCmdDFs',
+        'clientId': '145180067857-iff01u76igkocukv6j1hfvjboe1bf97c.apps.googleusercontent.com',
+        'discoveryDocs': [discoveryUrl],
+        'scope': SCOPE
+    }).then(function () {
+      GoogleAuth = gapi.auth2.getAuthInstance();
+
+      // Listen for sign-in state changes.
+      GoogleAuth.isSignedIn.listen(updateSigninStatus);
+
+      // Handle initial sign-in state. (Determine if user is already signed in.)
+      var user = GoogleAuth.currentUser.get();
+      setSigninStatus();
+ 
+    });
+  }
+
+  function handleAuthClick() {
+    if (GoogleAuth.isSignedIn.get()) {
+      // User is authorized and has clicked "Sign out" button.
+      GoogleAuth.signOut();
+    } else {
+      // User is not signed in. Start Google auth flow.
+      GoogleAuth.signIn();
+    }
+  }
+
+  function revokeAccess() {
+    GoogleAuth.disconnect();
+  }
+
+  function setSigninStatus() {
+    var user = GoogleAuth.currentUser.get();
+    var isAuthorized = user.hasGrantedScopes(SCOPE);
+    if (isAuthorized) {
+      console.log("AUTO"); 
+    } else {
+      console.log("NO AUTO"); 
+  }
+  }
+
+  function updateSigninStatus() {
+    setSigninStatus();
+  }
+  }; 
+
   return ( 
     <>
 
@@ -56,6 +117,8 @@ const Login = () => {
          <button onClick={facebookLogin} class="btn-login btn-fb btn-primary">CARGAR FB</button>  
          <button onClick={facebookStart} class="btn-login btn-g btn-primary">INICIAR FB</button> 
          <button onClick={googleLogin} class="btn-primary">CARGAR G</button> 
+         <button onClick={googleStart} class="btn-primary">CARGAR S</button>
+
       </div> 
    	 </div> 
    	 </div> 
